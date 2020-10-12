cce0cd9fd2155eddc76591765312f8e88fe6f3e3
Tomi Panula <tomipanula1@gmail.com>
2020-10-12 14:41:39 +0300

    added map

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import './App.css';
+import GoogleMapReact from 'google-map-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello world PWA demo
-        </p>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<div style={{ height: '100vh', width: '100%' }}>
+					<GoogleMapReact
+						bootstrapURLKeys={{
+							key: process.env.GOOGLE_MAPS_API_KEY,
+						}}
+						defaultCenter={{ lat: 60.185243, lng: 24.826074 }}
+						defaultZoom={15}
+					>
+						<div
+							lat={60.185243}
+							lng={24.826074}
+							style={{
+								width: 20,
+								height: 20,
+								borderWidth: 3,
+								borderColor: 'red',
+								borderStyle: 'solid',
+								borderRadius: 20,
+								backgroundColor: 'lightblue',
+							}}
+						/>
+					</GoogleMapReact>
+				</div>
+			</header>
+		</div>
+	);
+};
 
 export default App;
