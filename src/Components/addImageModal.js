c6e222cfd51d67f519dd43eebba18c6d0397eed2
Sofia Kimpimäki <kimpims@aalto.fi>
2020-10-21 14:14:22 +0300

    Add drag and drop functionality

--- src/Components/addImageModal.js
+++ src/Components/addImageModal.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import Box from '@material-ui/core/Box';
+import React, { useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import IconButton from '@material-ui/core/IconButton';
@@ -9,6 +8,7 @@ import MuiDialogActions from '@material-ui/core/DialogActions';
 import Typography from '@material-ui/core/Typography';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { useTheme, makeStyles, withStyles } from '@material-ui/core/styles';
+import {useDropzone} from 'react-dropzone';
 
 import CloseIcon from '@material-ui/icons/Close';
 import VerticalAlignTopIcon from '@material-ui/icons/VerticalAlignTop';
@@ -88,9 +88,40 @@ const AddImageModal = (props) => {
 	const classes = useStyles();
 	const theme = useTheme();
 	const [showTakePhoto, setShowTakePhoto] = React.useState(false);
+	const [files, setFiles] = React.useState([]);
+	const {getRootProps, getInputProps} = useDropzone({
+		accept: 'image/*',
+		onDrop: acceptedFiles => {
+			setFiles(acceptedFiles.map(file => Object.assign(file, {
+				preview: URL.createObjectURL(file)
+			})));
+		}
+	});
+
+	const handlePictures = () => {
+		files.forEach(file => {
+			/* TODO: Add to the firebase*/
+		})
+	};
 
 	// Use full screen dialog for smaller screens
 	const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+	const thumbs = files.map(file => (
+		<div key={file.name}>
+			<div>
+				<img
+					src={file.preview}
+					style={{height: '100px', width: '100px'}}
+				/>
+			</div>
+		</div>
+	));
+
+
+	useEffect(() => {
+		// Make sure to revoke the data uris to avoid memory leaks
+		files.forEach(file => URL.revokeObjectURL(file.preview));
+	}, [files]);
 
 	return (
 		<Dialog fullScreen={fullScreen} open={props.open}>
@@ -107,12 +138,23 @@ const AddImageModal = (props) => {
 						<Button variant="outlined" onClick={() => setShowTakePhoto(true)}>
 							Choose image
 						</Button>
-						<Box className={classes.box}>
+
+						<div {...getRootProps({className: 'dropzone'})} className={classes.box}>
+							<input {...getInputProps()} />
 							<VerticalAlignTopIcon className={classes.uploadIcon} />
 							<Typography className={classes.uploadText}>
 								Choose an image or drag and drop it here
 							</Typography>
-						</Box>
+						</div>
+						{thumbs &&
+						<aside>
+							{thumbs}
+						</aside>
+						}
+
+
+
+
 					</DialogContent>
 					<DialogActions>
 						<Button
@@ -121,12 +163,16 @@ const AddImageModal = (props) => {
 							color="primary"
 							className={classes.footerButton}
 							onClick={() => {
-								/* TODO */
+								handlePictures()
 							}}
 						>
 							Add
 						</Button>
-						<Button variant="outlined" onClick={() => props.handleClose()}>
+						<Button variant="outlined" onClick={() => {
+							setFiles([]);
+							props.handleClose()
+						}}
+						>
 							Cancel
 						</Button>
 					</DialogActions>
